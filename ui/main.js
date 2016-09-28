bcb0a345d4a1aa76788b2c9401ff4a16d4cec8c0
ManognaMutyala <manoghna10@gmail.com>
2016-09-28 22:19:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-txt');
 element.innerHTML='New value';
+var img=document.getElementById('madi');
+madi.onclick=function{
+   madi.style.margin='100px'; 
+};
