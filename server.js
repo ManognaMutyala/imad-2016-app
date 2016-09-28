f845d2543b04235a12bbd132f0ef5518839d0d83
ManognaMutyala <manoghna10@gmail.com>
2016-09-28 19:14:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,57 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var article1={
+    title:'article1',
+    heading:'articleone',
+};
+
+function createTemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var htmltemplate=< html> <head>
+        <title>
+            ${title}
+            
+        </title>
+        <meta name="viewport" content="width=device -width,initial scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+        <style>
+        </style>
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">HOME</a>
+        </div>
+        <div>
+            <hr/>
+            ${heading}
+            </div>
+            <div>
+                sep 28
+            </div>
+            <div>
+                <p>
+                    Its fun to learn webaapp and executing it!!!!!!!!!
+                </p>
+                <p>
+                    i want to get maximum out of this course
+                </p>
+            
+        </div>
+        </div>
+    </body>
+</html>;
+return htmlTemplate;
+}
+
+
+
+    
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
